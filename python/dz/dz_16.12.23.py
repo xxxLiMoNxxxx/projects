5ebbc47c0f67a7a5a067c26185e99d421a67ea15
xxxLiMoNxxxx <vladborozdin613@gmail.com>
2023-12-16 14:55:45 +0200

    init

--- python/dz/dz_16.12.23.py
+++ python/dz/dz_16.12.23.py
@@ -1,4 +1,4 @@
-"""#1 Завдання
+#1 Завдання
 print("\n 1 Завдання \n")
 pib = input("Введіть свій ПІБ: ")
 group = input("Введіть свою группу: ")
@@ -8,7 +8,7 @@ blood = input("Введіть свою група крові: ")
 students_dict = {pib : {"Группа":group,
                         "Вік":age,
                         "Группа крові": blood},}
-print(students_dict)"""
+print(students_dict)
 #2 Завдання
 print("\n 2 Завдання \n")
 all_contacts = []
@@ -20,16 +20,18 @@ for i in range(1, 11+1):
                                           "Номер контакта":number},}
     all_contacts.append(contacts_dict)
     while(True):
-        quastion = int(input("Що далі? \n 1 - додати новий контакт, 2 - подивитися всі контакти "))
-        if (quastion == 1):
+        quastion = input("Що далі? \n 1 - додати новий контакт, 2 - подивитися всі контакти ")
+        if (quastion == "1"):
             if(i == 10):
-                print("У вас вже максимально контактів")
+                print("\nУ вас вже максимально контактів\n")
             elif(i < 10):
                 break
-        elif (quastion == 2):
+        elif (quastion == "2"):
             if(i == 10):
                 print(all_contacts)
             elif(i < 10):
                 print(all_contacts)
                 break
+        else:
+            print("\nПомилка, спробуйте ще раз\n")
     
