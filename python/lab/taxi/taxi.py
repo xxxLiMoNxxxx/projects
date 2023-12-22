d07c9999d12ba1d9f15b4d1620ddcc89016ceb3a
xxxLiMoNxxxx <vladborozdin613@gmail.com>
2023-12-22 22:16:30 +0200

    added registration

--- python/lab/taxi/taxi.py
+++ python/lab/taxi/taxi.py
@@ -1,8 +1,33 @@
 import tkinter as tk
-from tkinter import *
-from tkinter import messagebox
+from tkinter import messagebox, PhotoImage
+
+root = tk.Tk()
 
 classes = []
+
+def show_message():
+    user_input1 = entry1.get()
+    user_input2 = entry2.get()
+    user_input3 = entry3.get()
+    if user_input1 and user_input2 and user_input3:
+        messagebox.showinfo("Повідомлення", f"Вітаю вас, {user_input3}!")
+        show_main_window()
+    else:
+        messagebox.showwarning("Помилка", "Будь ласка, заповніть всі поля.")
+
+def show_main_window():
+    registration_frame.pack_forget()
+    label_quastion.pack()
+    label_money.pack()
+    button_money.pack()
+    label_card.pack()
+    button_card.pack()
+
+def money_button_clicked():
+    clear_screen_and_message("Готівка")
+
+def card_button_clicked():
+    clear_screen_and_message("Карта")
 
 def clear_screen_and_message(selection):
     # Очистка екрана
@@ -38,12 +63,6 @@ def animate_loading(canvas, progress, end_progress, increment, label):
         messagebox.showinfo("Готово!", "Операція завершена")
         choose_classes_window()
 
-def money_button_clicked():
-    clear_screen_and_message("Готівка")
-
-def card_button_clicked():
-    clear_screen_and_message("Карта")
-
 def add_class(class_name):
     global classes
     if class_name not in classes:
@@ -71,34 +90,49 @@ def choose_classes_window():
     result_label.config(textvariable=selected_classes)
     result_label.pack()
 
-root = tk.Tk()
-
+# Настройка основного окна
 root.geometry("720x1280")
 root.minsize(50, 50)
 root.resizable(False, False)
 root.title("Застосунок")
 root.configure(bg="#f5da56")
 
-img_money = PhotoImage(file='python/lab/taxi/money.png')
-img_card = PhotoImage(file='python/lab/taxi/card.png')
+# Окно регистрации
+registration_frame = tk.Frame(root, bg="#f5da56")
+registration_frame.pack()
 
+label1 = tk.Label(registration_frame, text="Введіть логін", font="Arial 18", foreground="gray")
+entry1 = tk.Entry(registration_frame)
+label1.pack()
+entry1.pack()
+
+label2 = tk.Label(registration_frame, text="Введіть пароль", font="Arial 18", foreground="gray")
+entry2 = tk.Entry(registration_frame, show='*')
+label2.pack()
+entry2.pack()
+
+label3 = tk.Label(registration_frame, text="Введіть своє ім'я", font="Arial 18", foreground="gray")
+entry3 = tk.Entry(registration_frame)
+label3.pack()
+entry3.pack()
+
+button = tk.Button(registration_frame, text="OK", command=show_message)
+button.pack()
+
+# Элементы основного окна
 label_quastion = tk.Label(root, text="Яку ви хочете обрати форму оплати", font="Arial 20", foreground="black", bg="#f5da56")
-label_money = tk.Label(root, text="Готівка", font="Arial 13", foreground="black", bg="#f5da56")
-button_money = tk.Button(root, command=money_button_clicked)
+label_money = tk.Label(root, font="Arial 13", foreground="black", bg="#f5da56")
+button_money = tk.Button(root, text="", command=money_button_clicked)
+img_money = PhotoImage(file='python/lab/taxi/money.png')
 button_money.image = img_money
-button_money['image'] = button_money.image
+button_money.config(image=img_money, compound=tk.LEFT)
 label_card = tk.Label(root, text="Карта", font="Arial 13", foreground="black", bg="#f5da56")
 button_card = tk.Button(root, command=card_button_clicked)
+img_card = PhotoImage(file='python/lab/taxi/card.png')
 button_card.image = img_card
-button_card['image'] = button_card.image
+button_card.config(image=img_card, compound=tk.LEFT)
 
 selected_classes = tk.StringVar()
 result_label = tk.Label(root, font="Arial 13", foreground="black", bg="#f5da56")
-
-label_quastion.pack()
-label_money.pack()
-button_money.pack()
-label_card.pack()
-button_card.pack()
 
 root.mainloop()
